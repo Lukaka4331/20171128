0b8fb977f7253a6207e6639e04394bc9de781232
Lukaka4331 <105021044@live.asia.edu.tw>
2017-11-28 08:31:16 +0800

    rrfsfsd

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        CFrame cf = new CFrame();
+        cf.setVisible(true);
+        SFrame sf = new SFrame();
+        sf.setVisible(true);
+    }
+}
